30deb5d0d38fd1043a1152343936d66b3f60443c
TheRealImaginary <mazennnnnnnnn@gmail.com>
2018-11-29 21:13:45 +0200

    Added a Scripts folder and AudioManager Script

---
+++ Brick Wall Entertainment/Assets/Scripts/Audio/AudioManager.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using UnityEngine;
+
+namespace BrickWallEntertainment.Managers
+{
+    public class AudioManager : MonoBehaviour
+    {
+        private static AudioManager instance;
+
+        public static AudioManager Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public Sound[] sounds;
+
+        void Awake()
+        {
+            if (instance == null)
+                instance = this;
+            else if (instance != this)
+                Destroy(this.gameObject);
+            DontDestroyOnLoad(this.gameObject);
+
+            foreach (Sound sound in this.sounds)
+            {
+                AudioSource source = this.gameObject.AddComponent<AudioSource>();
+                sound.SetUpSource(source);
+            }
+        }
+
+        public void Play(string soundName)
+        {
+            Sound sound = this.findSoundByName(soundName);
+            if (sound != null)
+            {
+                sound.Play();
+            }
+        }
+
+        public void Pause(string soundName)
+        {
+            Sound sound = this.findSoundByName(soundName);
+            if (sound != null)
+            {
+                sound.Pause();
+            }
+        }
+
+        public void Stop(string soundName)
+        {
+            Sound sound = this.findSoundByName(soundName);
+            if (sound != null)
+            {
+                sound.Stop();
+            }
+        }
+
+        public void PauseAll()
+        {
+            foreach (Sound sound in this.sounds)
+            {
+                sound.Pause();
+            }
+        }
+
+        public void StopAll()
+        {
+            foreach (Sound sound in this.sounds)
+            {
+                sound.Stop();
+            }
+        }
+
+        private Sound findSoundByName(string soundName)
+        {
+            Sound sound = Array.Find(this.sounds, s => s.soundName.Equals(soundName));
+            if (sound == null)
+            {
+                Debug.LogWarning("Sound { " + soundName + " } not found !");
+            }
+            return sound;
+        }
+    }
+}
---
+++ Brick Wall Entertainment/Assets/Scripts/Audio/Sound.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace BrickWallEntertainment
+{
+
+    [System.Serializable]
+    public class Sound
+    {
+        public string soundName;
+
+        public AudioClip clip;
+
+        public AudioMixerGroup mixerGroup;
+
+        public bool playOnAwake;
+
+        public bool loop;
+
+        [Range(0.0f, 1.0f)]
+        public float volume;
+
+        [Range(-3.0f, 3.0f)]
+        public float pitch;
+
+        private AudioSource source;
+
+        public void SetUpSource(AudioSource source)
+        {
+            this.source = source;
+            this.source.clip = this.clip;
+            this.source.outputAudioMixerGroup = mixerGroup;
+            this.source.playOnAwake = this.playOnAwake;
+            this.source.loop = this.loop;
+            this.source.volume = this.volume;
+            this.source.pitch = this.pitch;
+        }
+
+        public void Play()
+        {
+            this.source.Play();
+        }
+
+        public void Pause()
+        {
+            if (this.source.isPlaying)
+            {
+                this.source.Pause();
+            }
+        }
+
+        public void Stop()
+        {
+            if (this.source.isPlaying)
+            {
+                this.source.Stop();
+            }
+        }
+    }
+}
