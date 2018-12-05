5d0f293c1182eceb69e6a7ced78f68f338fdd6b9
KarimWilliam <karim.william@gmail.com>
2018-12-05 22:11:55 +0200

    Pushing Level 1 and 2 basis.
    Still working on level 2. level 1 needs more details. scripts need to be edited to work with Kratos (things like healing him or killing him when a condition is met)

---
+++ Karim/Assets/Environment/Spikes/SpikeTrapScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpikeTrapScript : MonoBehaviour {
+    private Animator anim;
+    AudioSource audioSource;
+    public AudioClip SpikeTriggeredSound;
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+        anim = GetComponent<Animator>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag == "Kratos")
+        {
+            audioSource.PlayOneShot(SpikeTriggeredSound, 0.1F);
+            anim.Play("TriggeredSpikes");
+            //KILL KRATOS HERE
+        }
+    }
+}
