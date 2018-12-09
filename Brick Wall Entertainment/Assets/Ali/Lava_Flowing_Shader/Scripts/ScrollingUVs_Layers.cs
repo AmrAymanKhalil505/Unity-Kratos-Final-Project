5c4363caf274255925e69378faf9a7dbcf736870
Ali-ElBeheiry <beheiry_bb@hotmail.com>
2018-12-09 20:06:22 +0200

    Boss level done with moving parts, missing checking kratos jumps match with the level and lava logic if any

--- Brick Wall Entertainment/Assets/Ali/Lava_Flowing_Shader/Scripts/ScrollingUVs_Layers.cs
+++ Brick Wall Entertainment/Assets/Ali/Lava_Flowing_Shader/Scripts/ScrollingUVs_Layers.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class ScrollingUVs_Layers : MonoBehaviour 
 {
 	//public int materialIndex = 0;
-	public Vector2 uvAnimationRate = new Vector2( 1.0f, 0.0f );
+	public Vector2 uvAnimationRate = new Vector2( 0.001f, 0.001f );
 	public string textureName = "_MainTex";
 	
 	Vector2 uvOffset = Vector2.zero;
---
+++ Brick Wall Entertainment/Assets/Ali/Scripts/PlatformScript.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformScript : MonoBehaviour {
+    bool goingDown=true;
+    int counter=0;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void FixedUpdate()
+    {
+        if (this.tag == "FrontBackPlatforms")
+        {
+            if(counter >= 200)
+            {
+                goingDown = !goingDown;
+                counter = 0;
+            }
+            if (goingDown)
+            {
+                this.transform.Translate(0, -Time.deltaTime*1.15f, 0);
+            }
+            else
+            {
+                this.transform.Translate(0, Time.deltaTime*1.15f, 0);
+            }
+            counter++;
+        }
+
+        if (this.tag == "RightLeftPlatforms")
+        {
+            if (counter >= 200)
+            {
+                goingDown = !goingDown;
+                counter = 0;
+            }
+            if (!goingDown)
+            {
+                this.transform.Translate(0, -Time.deltaTime*1.15f, 0);
+            }
+            else
+            {
+                this.transform.Translate(0, Time.deltaTime*1.15f, 0);
+            }
+            counter++;
+        }
+
+    }
+}
