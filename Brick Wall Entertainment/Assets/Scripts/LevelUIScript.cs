f1744e3e9e37297857dd911a0bce5926c95c83cf
GehadAbdElhalim <halimgego@gmail.com>
2018-12-14 20:58:08 +0200

    kratos saves attributes across normal and boss scene

---
+++ Brick Wall Entertainment/Assets/Scripts/LevelUIScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelUIScript : MonoBehaviour {
+
+	private PlayerController Kratos;
+
+	// Use this for initialization
+	void Start () {
+		Kratos = GameObject.FindGameObjectWithTag("Kratos").GetComponent<PlayerController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.GetComponent<Text>().text = Kratos.level.ToString();
+	}
+}
